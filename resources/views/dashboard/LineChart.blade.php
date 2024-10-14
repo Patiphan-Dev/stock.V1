4667abd60bd0cf5400ddc0559444c70bb0061e41
Patiphan-Dev <patiphan.dev@gmail.com>
2024-10-15 03:53:17 +0700

    up

--- resources/views/dashboard/LineChart.blade.php
+++ resources/views/dashboard/LineChart.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-12">
     <div class="card card-info">
         <div class="card-header">
-            <h3 class="card-title">Line Chart: สินค้าขายดีในช่วง 1 ปี</h3>
+            <h3 class="card-title">กราฟแสดงสินค้าขายดีในช่วง 1 ปี</h3>
         </div>
         <div class="card-body">
             <div class="chart">
